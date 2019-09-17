3256b2f50fb1f021878af02bfb53ed65f0b1e25d
Jayd <aimejayd2016@gmail.com>
2019-09-17 14:27:39 +0200

    My first assignment

---
+++ number1.py
@@ -0,0 +1,21 @@
+#Names: Manishimwe Jean De Dieu
+
+#The program below is code inorder to print out the first letters of my names in big letters
+
+
+
+print ("MMM               MMM                     JJJJJJJJJJJJJJJJJJJ")
+print ("M   M           M    M                             J ")
+print ("M    M         M     M                             J ")
+print ("M     M       M      M                             J ")
+print ("M      M     M       M                             J ")
+print ("M       M   M        M                             J ")
+print ("M        M M         M                             J ")
+print ("M                    M                             J ")
+print ("M                    M                             J ")
+print ("M                    M                             J ")
+print ("M                    M                            J  ")
+print ("M                    M                           J  ")
+print ("M                    M            JJJJJJJJJJJJJJ ")
+
+
